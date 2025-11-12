4b96bca3b80bde837a5720b8eb002a30654f77e6
Craig Laws <craig.laws@zonal.co.uk>
2025-11-12 21:36:51 +0000

    Update logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,4 +1,4 @@
-import logo from '../assets/liam.png';
+import logo from '../assets/logo.png';
 
 export const Logo: React.FC = () => {
   return <img src={logo.src} alt="Liam Florence Video Logo" className="logo" />;
